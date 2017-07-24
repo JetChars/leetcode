05066de332bc7b4020b570dd95e27734151adc47
JetChars <calebjay@live.cn>
2017-07-25 00:36:00 +0800

    add two numbers w/o operator + or -

---
+++ cpp/0002_add_two_numbers/add_two_numbers.cc
@@ -0,0 +1,29 @@
+// C/C++ File
+// AUTHOR:   jetchars
+// FILE:     0002_add_two_numbers/add_two_numbers.cc
+// ROLE:     TODO (some explanation)
+// CREATED:  2017-07-25 00:26:29
+// MODIFIED: 2017-07-25 00:34:22
+
+/* You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list. */
+
+/* You may assume the two numbers do not contain any leading zero, except the number 0 itself. */
+
+/* Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) */
+/* Output: 7 -> 0 -> 8 */
+
+#include <cstddef>
+
+/* Definition for singly-linked list. */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+    }
+};
